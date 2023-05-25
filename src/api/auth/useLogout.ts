2063ab0327c955ee21bfc18dce8458194ee4d4f5
Vladislav Nikolaev <vladisnotlove@gmail.com>
2023-05-25 19:03:29 +0300

    Fix logout

--- src/api/auth/useLogout.ts
+++ src/api/auth/useLogout.ts
@@ -3,11 +3,13 @@ import urls from "Api/urls";
 import queryKeys from "Api/queryKeys";
 import {post} from "Api/@core/methods";
 import {handleApiCatch, TApiErrors} from "Api/@core/errors";
+import {useRouter} from "next/router";
 
 type TLogoutResult = undefined;
 type TLogoutError = TApiErrors;
 
 const useLogout = () => {
+	const {reload} = useRouter();
 	const queryClient = useQueryClient();
 
 	return useMutation<TLogoutResult, TLogoutError>(
@@ -16,6 +18,7 @@ const useLogout = () => {
 			return post<TLogoutResult>(urls.auth_logout())
 				.then(response => {
 					queryClient.invalidateQueries(queryKeys.profile());
+					reload();
 					return response.data;
 				})
 				.catch(handleApiCatch);
